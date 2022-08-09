630a51bc2759ed35153acc9afc4a1abd3c485964
lem0n4de <yunus.emre.orcun@outlook.com>
2022-08-09 20:22:32 +0300

    add Request, Url, RequestParameters, utils for url encode decode and some tests
    Signed-off-by: lem0n4de <yunus.emre.orcun@outlook.com>

---
+++ request.h
@@ -0,0 +1,60 @@
+// MIT License
+//
+// Copyright (c) 2022 Yunus Emre ÖRCÜN
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef SCRAPP_REQUEST_H
+#define SCRAPP_REQUEST_H
+
+#include <cpr/cpr.h>
+#include "utils.h"
+
+
+namespace Scrapp {
+    using Url = cpr::Url;
+
+    class RequestParameter {
+    public:
+        RequestParameter(const std::string& key, const std::string& value) : _key(Scrapp::url_encode(key)),
+                                                                             _value(Scrapp::url_encode(value)) {};
+        std::string key() { return this->_key; }
+        std::string value() { return this->_value; }
+    private:
+        const std::string _key;
+        const std::string _value;
+    };
+
+    class Request {
+    public:
+        Request() = default;
+        explicit Request(Url url) : _url(std::move(url)) {};
+        template<typename ParameterContainer>
+        Request(Url url, ParameterContainer params) : _url(url) {
+            for (auto& param: params) {
+                this->_parameters.push_back(param);
+            }
+        }
+    private:
+        Url _url;
+        std::vector<RequestParameter> _parameters;
+    };
+}
+
+#endif //SCRAPP_REQUEST_H
---
+++ utils.h
@@ -0,0 +1,82 @@
+// MIT License
+//
+// Copyright (c) 2022 Yunus Emre ÖRCÜN
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef SCRAPP_UTILS_H
+#define SCRAPP_UTILS_H
+
+#include <cctype>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+namespace Scrapp {
+    std::string url_encode(const std::string& value) {
+        std::ostringstream escaped;
+        escaped.fill('0');
+        escaped << std::hex;
+
+        for (char c: value) {
+            // Keep alphanumeric and other accepted characters intact
+            if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
+                escaped << c;
+                continue;
+            }
+
+            // Any other characters are percent-encoded
+            escaped << std::uppercase;
+            escaped << '%' << std::setw(2) << int((unsigned char) c);
+            escaped << std::nouppercase;
+        }
+
+        return escaped.str();
+    }
+
+    char from_hex(char ch) {
+        return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
+    }
+
+    std::string url_decode(std::string text) {
+        char h;
+        std::ostringstream escaped;
+        escaped.fill('0');
+
+        for (auto i = text.begin(), n = text.end(); i != n; ++i) {
+            std::string::value_type c = (*i);
+
+            if (c == '%') {
+                if (i[1] && i[2]) {
+                    h = from_hex(i[1]) << 4 | from_hex(i[2]);
+                    escaped << h;
+                    i += 2;
+                }
+            } else if (c == '+') {
+                escaped << ' ';
+            } else {
+                escaped << c;
+            }
+        }
+
+        return escaped.str();
+    }
+}
+
+#endif //SCRAPP_UTILS_H
